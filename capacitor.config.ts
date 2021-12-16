360583dfb524ce1a056c2c56b6daf63f3b90182c
Rintu Raj.c <rinturajc180000@gmail.com>
2021-12-17 00:58:52 +0530

    ionic upgraded to 6

---
+++ capacitor.config.ts
@@ -0,0 +1,27 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wacko',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
+};
+
+export default config;
--- src/app/startup/signin/signin.component.ts
+++ src/app/startup/signin/signin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { GunDb } from 'src/app/services/gun-db.service';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { LoadingController } from '@ionic/angular';
 
 
 @Component({
@@ -14,7 +16,7 @@ export class SigninComponent implements OnInit {
 
 
   err
-  constructor(private db: GunDb,private router:Router) {
+  constructor(private db: GunDb, private router: Router, public loadingController: LoadingController) {
 
 
 
@@ -23,23 +25,51 @@ export class SigninComponent implements OnInit {
   ngOnInit() {
 
   }
-  login(value) {
+  async login(value) {
 
-    //  this.db.user.create(value.username, value.password, (err:any)=>{
-    //    if(err){
-    //     alert(err.err)
+    // this.db.user.create(value.username, value.password, (err: any) => {
+    //   if (err) {
+
+
+    //     if (err.err == 'User already created!') {
+    //       this.err = ''
+    //       this.db.user.auth(value.username, value.password, (ack: any) => {
+    //         if (ack.err) {
+    //           this.err = ack.err
+    //         } else {
+    //           this.router.navigateByUrl('home')
+    //         }
+    //       })
+    //     }
     //     console.log(err);
-    //    }else{
-    this.err = ''
+    //   } else {
+    //     this.err = ''
+
+
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+      animated:true
+    });
+    await loading.present();
+
+  
+    const hapticsImpactLight = async () => {
+      await Haptics.impact({ style: ImpactStyle.Light });
+    };
+ 
+
     this.db.user.auth(value.username, value.password, (ack: any) => {
+      loading.dismiss()
       if (ack.err) {
         this.err = ack.err
+        hapticsImpactLight()
       } else {
+ 
         this.router.navigateByUrl('home')
       }
     })
-    //    }
-    //  })
+    // }
+    // })
 
   }
 
